79b2499246e84eb50b5df1baf7aca3a4b8939484
greenghostman <server02.psa1981@gmail.com>
2019-05-27 00:08:09 +0630

    Clean Arduino

--- chefbot/Arduino/motor_controller_pwm_only/messenger.ino
+++
@@ -1,7 +0,0 @@
-void setupMessenger(){
-  //setupMessenger();
-   Serial.begin(115200); 
-  cmdMessenger.printLfCr();  
-  attachCommandCallbacks();
-  cmdMessenger.sendCmd(Start,"Arduino has started!");  
-}
--- chefbot/Arduino/motor_controller_pwm_only/motor.ino
+++
@@ -1,9 +0,0 @@
-void setupMotors(){
-  AFMS.begin();  // create with the default frequency 1.6KHz   
-  motor_right->setSpeed(0);
-  motor_left->setSpeed(0);
-  motor_right->run(FORWARD);
-  motor_right->run(RELEASE);
-  motor_left->run(FORWARD);
-  motor_left->run(RELEASE);
-}
