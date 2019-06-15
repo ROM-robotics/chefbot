650461ab00c83b743c389e4e1420777a5563c085
greenghostman <server02.psa1981@gmail.com>
2019-06-15 10:32:31 +0630

    remove ekf and best encoder tuning

--- chefbot_bringup/scripts/arduino_node_with_gyro_accel_imu_remove_mpu_serial.py
+++ chefbot_bringup/scripts/arduino_node_with_gyro_accel_imu_remove_mpu_serial.py
@@ -53,37 +53,7 @@ class Arduino_Class(object):
 
 		self._Counter=0
 
-		self.imu = Imu()
-		self.imu.header.frame_id = "imu"
-		self.imu.orientation_covariance[0] = 1e6;
-		self.imu.orientation_covariance[1] = 0;
-		self.imu.orientation_covariance[2] = 0;
-		self.imu.orientation_covariance[3] = 0;
-		self.imu.orientation_covariance[4] = 1e6;
-		self.imu.orientation_covariance[5] = 0;
-		self.imu.orientation_covariance[6] = 0;
-		self.imu.orientation_covariance[7] = 0;
-		self.imu.orientation_covariance[8] = 1e-6;
 
-		self.imu.angular_velocity_covariance[0] = 1e6;
-		self.imu.angular_velocity_covariance[1] = 0;
-		self.imu.angular_velocity_covariance[2] = 0;
-		self.imu.angular_velocity_covariance[3] = 0;
-		self.imu.angular_velocity_covariance[4] = 1e6;
-		self.imu.angular_velocity_covariance[5] = 0;
-		self.imu.angular_velocity_covariance[6] = 0;
-		self.imu.angular_velocity_covariance[7] = 0;
-		self.imu.angular_velocity_covariance[8] = 1e-6;
-
-		self.imu.linear_acceleration_covariance[0] = -1;
-		self.imu.linear_acceleration_covariance[1] = 0;
-		self.imu.linear_acceleration_covariance[2] = 0;
-		self.imu.linear_acceleration_covariance[3] = 0;
-		self.imu.linear_acceleration_covariance[4] = 0;
-		self.imu.linear_acceleration_covariance[5] = 0;
-		self.imu.linear_acceleration_covariance[6] = 0;
-		self.imu.linear_acceleration_covariance[7] = 0;
-		self.imu.linear_acceleration_covariance[8] = 0;
 
 		self.vector3 = Vector3Stamped()
 #######################################################################################################################
@@ -104,8 +74,8 @@ class Arduino_Class(object):
 		self._Req_subscriber = rospy.Subscriber('rpm_req_msg',rpm,self._Handle_rpm_request)
 
 		self._Act_publisher = rospy.Publisher('rpm_act_msg',Vector3Stamped,queue_size = 10)
-		self._Imu_publisher = rospy.Publisher('mpu9250_imu',Imu,queue_size=10)
-		self._SerialPublisher = rospy.Publisher('serial', String,queue_size=10)
+		#self._Imu_publisher = rospy.Publisher('mpu9250_imu',Imu,queue_size=10)
+		#self._SerialPublisher = rospy.Publisher('serial', String,queue_size=10)
 
 
 
@@ -126,7 +96,7 @@ class Arduino_Class(object):
 
 	def _HandleReceivedLine(self,  line):
 		self._Counter = self._Counter + 1
-		self._SerialPublisher.publish(String(str(self._Counter) + ", in:  " + line))
+		#self._SerialPublisher.publish(String(str(self._Counter) + ", in:  " + line))
 		#rospy.loginfo("hello1")
 
 		if(len(line) > 0):
@@ -158,13 +128,6 @@ class Arduino_Class(object):
 					self.acce_Z = float(li[6])
 					#rospy.loginfo("oK i got imu")
 
-					self.imu.angular_velocity.x = self.gyro_X
-					self.imu.angular_velocity.y = self.gyro_Y
-					self.imu.angular_velocity.z = self.gyro_Z
-					self.imu.linear_acceleration.x = self.acce_X
-					self.imu.linear_acceleration.y = self.acce_Y
-					self.imu.linear_acceleration.z = self.acce_Z
-					self._Imu_publisher.publish(self.imu)
 
 			except:
 				#rospy.logwarn("Error in Sensor values")
@@ -177,7 +140,7 @@ class Arduino_Class(object):
 
 
 	def _WriteSerial(self, message):
-		self._SerialPublisher.publish(String(str(self._Counter) + ", out: " + message))
+		#self._SerialPublisher.publish(String(str(self._Counter) + ", out: " + message))
 		self._SerialDataGateway.Write(message)
 
 #######################################################################################################################
