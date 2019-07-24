54bcf90c2a3ac8443a7f299abe06f8515f22a271
greenghostman <server02.psa1981@gmail.com>
2019-07-24 11:53:22 +0630

    change base_width and robot radius

--- chefbot_bringup/src/robot_specs.h
+++ chefbot_bringup/src/robot_specs.h
@@ -4,19 +4,16 @@
 #define encoder_pulse   11
 #define gear_ratio      30
 #define wheel_diameter  0.069   // meter
-#define track_width     0.244   // meter --> 24.4cm
+#define track_width     0.211   // meter ////edit
 #define MAX_RPM         320 /// edit
 #define pi              3.1415926
 #define two_pi          6.2831853
+#define new_enc_ticks   1320
 #endif
-
-
-
 
 // ----------------- BACKUP ------------------- //
 // #ifndef ROBOT_SPECS_H
 // #define ROBOT_SPECS_H
-
 // #define encoder_pulse   13
 // #define gear_ratio      120
 // #define wheel_diameter  0.069   //m
---
+++ chefbot_gui/scripts/Readme.txt
@@ -0,0 +1,14 @@
+sudo apt-get install python
+
+or 
+
+sudo apt-get install python3
+
+
+-------------------------------------------------
+
+sudo apt-get install python-tk
+
+or
+
+sudo apt-get install python3-tk
