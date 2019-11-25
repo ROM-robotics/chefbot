bfc64279fcb7ea8444e4eb2c4effa40a7c72c9bb
pyaesoanaung <server01.psa1981@gmail.com>
2019-11-25 16:03:11 +0000

    clean version

---
+++ dependency.sh
@@ -0,0 +1,16 @@
+////////
+ros package 
+
+chefbot
+sudo apt install ros-kinetic-urdf
+sudo apt install ros-kinetic-jsk-gui-msgs
+sudo apt install ros-kinetic-tf
+sudo apt install ros-kinetic-move-base-msgs
+sudo apt install ros-kinetic-xacro
+sudo apt install ros-kinetic-salm-gmapping ros-kinetic-amcl ros-kinetic-move-base
+sudo apt install ros-kinetic-rplidar-ros
+sudo apt install ros-kinetic-rosserial-python ros-kinetic-rosserial-arduino
+sudo apt install ros-kinetic-tf2-geometry-msgs
+
+git clone --branch pi3-devel https://github.com/greenghostman/chefbot
+
