473dae53835d259a0678bda762e2f69458f8910d
Pyae Soan Aung <server02.psa1981@gmail.com>
2019-11-26 14:51:01 +0630

    change depend

--- dependency.sh
+++ dependency.sh
@@ -3,11 +3,11 @@ ros package
 
 chefbot
 sudo apt install ros-kinetic-urdf
-sudo apt install ros-kinetic-jsk-gui-msgs
+sudo apt install ros-kinetic-map-server
 sudo apt install ros-kinetic-tf
-sudo apt install ros-kinetic-move-base-msgs
+sudo apt install ros-kinetic-move-base-msgs ros-kinetic-move-base
 sudo apt install ros-kinetic-xacro
-sudo apt install ros-kinetic-salm-gmapping ros-kinetic-amcl ros-kinetic-move-base
+sudo apt install ros-kinetic-salm-gmapping ros-kinetic-amcl ros-kinetic-dwa-local-planner
 sudo apt install ros-kinetic-rplidar-ros
 sudo apt install ros-kinetic-rosserial-python ros-kinetic-rosserial-arduino
 sudo apt install ros-kinetic-tf2-geometry-msgs
